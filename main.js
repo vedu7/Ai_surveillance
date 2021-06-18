a751e7c0bf95aa1bd865eaa715a4aab1de5c790d
vedu7 <75482297+vedu7@users.noreply.github.com>
2021-06-18 14:30:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+video = "";
+objects = [];
+status = "";
+function preload() {
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if(status != ""){
+objectDetector.detect(video,gotResult);
+for(i = 0;i < objects.length; i++){
+document.getElementById("status").innerHTML = "Status : Objects Detected";
+document.getElementById("number_of_objects").innerHTML = "Number Of Objects Detected : " + objects.length;
+fill("#ff0000");
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + " " + percent + "%" ,  objects[i].x + 15 , objects[i].y + 15);
+noFill();
+stroke(255, 0, 0);
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+}
+    }
+}
+
+ function start(){
+     objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+     document.getElementById("status").innerHTML = "Status: Detecting Objects";
+ }
+
+function modelLoaded(){
+    console.log("Cocossd is intialized");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+//Coco Single shot multibox detection
+
